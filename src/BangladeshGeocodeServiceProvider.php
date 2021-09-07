1d671d417b56816adf3f9e2545e982ac964b29ac
Lemon Patwari <lemonpatwari@gmail.com>
2021-09-07 22:21:15 +0600

    published tag add

--- src/BangladeshGeocodeServiceProvider.php
+++ src/BangladeshGeocodeServiceProvider.php
@@ -23,11 +23,11 @@ class BangladeshGeocodeServiceProvider extends ServiceProvider
 
             $this->publishes([
                 __DIR__.'/../database/migrations' => database_path('migrations'),
-            ]);
+            ],'lp-bangladesh-geocode-migrations');
 
             $this->publishes([
                 __DIR__.'/../database/seeders' => database_path('seeders'),
-            ]);
+            ],'lp-bangladesh-geocode-seeders');
 
         }
 
